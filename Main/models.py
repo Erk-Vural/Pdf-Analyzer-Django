14e4728d5c66321666e800d231df29547c6e49ea
Erk-Vural <erk.vural@gmail.com>
2021-12-02 17:20:08 +0300

    0.0.2 - Creating model for admin and user

--- Main/models.py
+++ Main/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Admin(models.Model):
+    class Meta:
+        db_table = "admin"
+
+    username = models.CharField(max_length=32)
+    password = models.CharField(max_length=32)
+
+    def __str__(self):
+        return self.username
+
+
+class User(models.Model):
+    class Meta:
+        db_table = "user"
+
+    username = models.CharField(max_length=32)
+
+    def __str__(self):
+        return self.username
+
